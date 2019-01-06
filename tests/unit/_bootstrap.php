0020eedf323421224c56569a64ab1e672705b0e0
keputh <36189033+keputh@users.noreply.github.com>
2019-01-06 21:03:28 +0300

    потом поправлю т_т

--- tests/unit/_bootstrap.php
+++ tests/unit/_bootstrap.php
@@ -7,12 +7,7 @@
  */
 require_once 'C:\Users\Дмитрий\Desktop\программирование\OSPanel\domains\redmine\application\core\abstractFactory.php';
 require_once 'C:\Users\Дмитрий\Desktop\программирование\OSPanel\domains\redmine\application/core/model.php';
-require_once 'C:\Users\Дмитрий\Desktop\программирование\OSPanel\domains\redmine\application/core/view.php';
-include 'C:\Users\Дмитрий\Desktop\программирование\OSPanel\domains\redmine\application/core/registry.php';
-require_once 'C:\Users\Дмитрий\Desktop\программирование\OSPanel\domains\redmine\vendor/autoload.php';
 
+include 'C:\Users\Дмитрий\Desktop\программирование\OSPanel\domains\redmine\application/core/money.php';
 include 'C:\Users\Дмитрий\Desktop\программирование\OSPanel\domains\redmine\application/models/dollar.php';
-
-
-require_once 'C:\Users\Дмитрий\Desktop\программирование\OSPanel\domains\redmine\application/core/controller.php';
-require_once 'C:\Users\Дмитрий\Desktop\программирование\OSPanel\domains\redmine\application/core/route.php';
+include 'C:\Users\Дмитрий\Desktop\программирование\OSPanel\domains\redmine\application/models/franc.php';
