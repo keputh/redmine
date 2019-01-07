11cc9be372402f6c7a57e5fbafdd9d354c5139d6
keputh <36189033+keputh@users.noreply.github.com>
2019-01-07 13:38:54 +0300

    Создал тест для модели пользователь, пока пустой

---
+++ tests/unit/ModelsTest.php
@@ -0,0 +1,22 @@
+<?php 
+class ModelsTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+    
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testUser()
+    {
+        
+    }
+}
