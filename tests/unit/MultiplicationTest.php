1e16c9d1fa19cb318ff064b6f396e508bbdafab0
keputh <36189033+keputh@users.noreply.github.com>
2019-01-07 13:43:21 +0300

    Создаю теперь объекты доллара и франка статичными методами(фабрики) из родительского класса money.

--- tests/unit/MultiplicationTest.php
+++ tests/unit/MultiplicationTest.php
@@ -19,23 +19,23 @@ class MultiplicationTest extends \Codeception\Test\Unit
     public function testMultiplication()
     {
         $five = Money::dollar(5);
-        $this->assertEquals(new Models_dollar(10), $five->times(2));
-        $this->assertEquals(new Models_dollar(15), $five->times(3));
+        $this->assertEquals(Money::dollar(10), $five->times(2));
+        $this->assertEquals(Money::dollar(15), $five->times(3));
     }
 
     public function testFrancMultiplication()
     {
-        $five = new Models_franc(5);
-        $this->assertEquals(new Models_franc(10), $five->times(2));
-        $this->assertEquals(new Models_franc(15), $five->times(3));
+        $five = Money::franc(5);
+        $this->assertEquals(Money::franc(10), $five->times(2));
+        $this->assertEquals(Money::franc(15), $five->times(3));
     }
 
     public function testEquality()
     {
-        $this->assertTrue((new Models_dollar(5))->equals(new Models_dollar(5)));
-        $this->assertFalse((new Models_dollar(5))->equals(new Models_dollar(6)));
-        $this->assertTrue((new Models_franc(5))->equals(new Models_franc(5)));
-        $this->assertFalse((new Models_franc(5))->equals(new Models_franc(6)));
+        $this->assertTrue(Money::dollar(5)->equals(Money::dollar(5)));
+        $this->assertFalse(Money::dollar(5)->equals(Money::dollar(6)));
+        $this->assertTrue(Money::franc(5)->equals(Money::franc(5)));
+        $this->assertFalse(Money::franc(5)->equals(Money::franc(6)));
     }
 
 }
