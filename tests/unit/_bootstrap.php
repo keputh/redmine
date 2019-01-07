ed1ec2848d2d931cf3440bcd7e5088ff48dee32c
keputh <36189033+keputh@users.noreply.github.com>
2019-01-07 13:41:40 +0300

    Поправлю потом т_Т

--- tests/unit/_bootstrap.php
+++ tests/unit/_bootstrap.php
@@ -5,9 +5,10 @@
  * Date: 05.01.2019
  * Time: 14:29
  */
-require_once 'C:\Users\Дмитрий\Desktop\программирование\OSPanel\domains\redmine\application\core\abstractFactory.php';
 require_once 'C:\Users\Дмитрий\Desktop\программирование\OSPanel\domains\redmine\application/core/model.php';
 
 include 'C:\Users\Дмитрий\Desktop\программирование\OSPanel\domains\redmine\application/core/money.php';
 include 'C:\Users\Дмитрий\Desktop\программирование\OSPanel\domains\redmine\application/models/dollar.php';
 include 'C:\Users\Дмитрий\Desktop\программирование\OSPanel\domains\redmine\application/models/franc.php';
+
+include 'C:\Users\Дмитрий\Desktop\программирование\OSPanel\domains\redmine\application/models/user.php';
