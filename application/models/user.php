a2db853b28e2f26a18f33ab094daa9133457a080
keputh <36189033+keputh@users.noreply.github.com>
2019-01-08 20:08:28 +0300

    без изменений

--- application/models/user.php
+++ application/models/user.php
@@ -2,6 +2,6 @@
 
 class Models_User extends Model_DB {
 
-	public $_fields = ['user_id', 'name', 'surname', 'email', 'position'];
+	protected $_fields = ['user_id', 'name', 'surname', 'email', 'position'];
 	
 }
