9118faeb9bb84de83b7e71fe7331a7d8fd23141b
keputh <36189033+keputh@users.noreply.github.com>
2019-01-09 23:12:46 +0300

    Создал тестовую базу данных, создал подключение к тестовой базе в папке тестов Подключил класс в _bootstrap

--- tests/unit/_bootstrap.php
+++ tests/unit/_bootstrap.php
@@ -5,11 +5,13 @@
  * Date: 05.01.2019
  * Time: 14:29
  */
+include 'connect.php';
 require_once 'C:\Users\Дмитрий\Desktop\программирование\OSPanel\domains\redmine\application/core/model.php';
-
 include 'C:\Users\Дмитрий\Desktop\программирование\OSPanel\domains\redmine\application/core/money.php';
+
 include 'C:\Users\Дмитрий\Desktop\программирование\OSPanel\domains\redmine\application/models/dollar.php';
 include 'C:\Users\Дмитрий\Desktop\программирование\OSPanel\domains\redmine\application/models/franc.php';
+
 
 include 'C:\Users\Дмитрий\Desktop\программирование\OSPanel\domains\redmine\application/models/create.php';
 include 'C:\Users\Дмитрий\Desktop\программирование\OSPanel\domains\redmine\application/models/user.php';
---
+++ tests/unit/connect.php
@@ -0,0 +1,10 @@
+<?php
+Class Connect
+{
+    
+    public static function connectDB()
+    {
+        return new mysqli("127.0.0.1", "root", "", "tests");
+    }
+    
+}
