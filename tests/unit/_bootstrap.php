077f817c72aa092fee017780440c20c4c0e59649
keputh <36189033+keputh@users.noreply.github.com>
2019-01-12 16:48:54 +0300

    1.Скопировал данные из основнова будстрап файла в тестовый будстрап. 2.для указания пути к рабочему файлу, добавил путь в отдельную переменную cd 3.Скорректировал вывод сторонних файлов, добавив переменную cd

--- tests/unit/_bootstrap.php
+++ tests/unit/_bootstrap.php
@@ -5,14 +5,29 @@
  * Date: 05.01.2019
  * Time: 14:29
  */
+
+$cd = 'C:\Users\Дмитрий\Desktop\программирование\OSPanel\domains\redmine\application/';
 include 'connect.php';
-require_once 'C:\Users\Дмитрий\Desktop\программирование\OSPanel\domains\redmine\application/core/model.php';
-include 'C:\Users\Дмитрий\Desktop\программирование\OSPanel\domains\redmine\application/core/money.php';
+require_once $cd .'core/model.php';
+require_once $cd .'core/money.php';
+require_once $cd .'core/view.php';
+include      $cd .'core/registry.php';
 
-include 'C:\Users\Дмитрий\Desktop\программирование\OSPanel\domains\redmine\application/models/dollar.php';
-include 'C:\Users\Дмитрий\Desktop\программирование\OSPanel\domains\redmine\application/models/franc.php';
+spl_autoload_register(function($className) {
+    $classParts = explode('_',$className);
 
+    $path = 'C:\Users\Дмитрий\Desktop\программирование\OSPanel\domains\redmine\application/';
+    foreach ($classParts as $part) {
+        $path .= '/'. $part;
+    }
+    $path .= '.php';
 
-include 'C:\Users\Дмитрий\Desktop\программирование\OSPanel\domains\redmine\application/models/create.php';
-include 'C:\Users\Дмитрий\Desktop\программирование\OSPanel\domains\redmine\application/models/user.php';
-include 'C:\Users\Дмитрий\Desktop\программирование\OSPanel\domains\redmine\application/models/task.php';
+    if (is_file($path)) {
+        include $path;
+    }
+});
+
+require_once $cd .'core/controller.php';
+require_once $cd .'controllers/controller_tasks.php';
+require_once $cd .'core/route.php';
+
