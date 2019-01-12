85fa03185b4d0774a4b4df5baf6117f89b2e2ba6
keputh <36189033+keputh@users.noreply.github.com>
2019-01-12 16:51:20 +0300

    Копипаст с основного коннекта

--- tests/unit/connect.php
+++ tests/unit/connect.php
@@ -1,10 +1,28 @@
 <?php
 Class Connect
 {
-    
+    /**
+     * @return mysqli - Подключаемся к базе
+     */
     public static function connectDB()
     {
         return new mysqli("127.0.0.1", "root", "", "tests");
+    }
+
+    /**
+     * Проверяем произошли ли какие либо изменения в базе
+     * @param $i - получаем функцию проверяющую изменения в базе
+     * @return string - успех, если есть изменения в базе
+     */
+    public static function infoConnect($i)
+    {
+        if($i > 0) {
+            return 'Запрос успешен';
+        }
+
+        else {
+            return 'Запрос не успешен';
+        }
     }
     
 }
