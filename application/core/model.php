c43355efd492f6a825681b872c6761c5099f3e2a
keputh <36189033+keputh@users.noreply.github.com>
2019-01-12 17:03:45 +0300

    1. Перенес переменную date и конструктор в объекты. 2. добавил Connect::infoConnect, проверяет успешен ли запрос, возвращает соответствующую строку.

--- application/core/model.php
+++ application/core/model.php
@@ -2,60 +2,49 @@
 include 'interfaceModel.php';
 
 class Model_DB implements Model
-{	
-    public $_data = [];
-
-	public function __construct($data) 
-	{
-		foreach ($this->_fields as $field) {
-			if (!empty($data[$field])) {
-		        $this->_data[$field] = $data[$field];
-			}
-	    }
-	}
-
+{
+	protected $_fields  = [];
+	
 	/**
-	 *  Записываем объект в базу
-	 *
-	 * @param string $table - получаем название таблицы
-	 * @param array $data - данные которые записываем
-	 *
+	 * @param $table
+	 * @param array $data
+	 * @return string
 	 */
     public static function add($table, array $data)
     {
 		$conny = Connect::connectDB();
 		$conny->query("INSERT into ". $table ." values(NULL,". "'" . implode($data, "', '") . "'" .")");
+        $result = Connect::infoConnect($conny->affected_rows);
 		$conny->close();
-		return;
+		return $result ;
     }
 
 	/**
-	 * редактируем объект в базе
-	 * @param string $table
+	 * @param $table
 	 * @param array $data
-	 *
+	 * @return string
 	 */
 	public static function edit($table, array $data)
 	{
 		$conny = Connect::connectDB();
 		$conny->query("replace into ". $table ." values(". "'" . implode("', '", $data) . "'" .")");
+		$result = Connect::infoConnect($conny->affected_rows);
 		$conny->close();
-		return;
+		return $result ;
 	}
-	
+
 	/**
-	 * Удаляем объект
-	 *
-	 * @param string $table - получаем название таблицы
-	 * @param string $id - получаем id объекта
-	 *
+	 * @param $table
+	 * @param $id
+	 * @return string
 	 */
 	public static function remove($table, $id)
 	{
 		$conny = Connect::connectDB();
 		$conny->query("delete from ". $table ." where user_id = ". $id ." ");
+		$result = Connect::infoConnect($conny->affected_rows);
 		$conny->close();
-		return;
+		return $result ;
 	}
 
 }
